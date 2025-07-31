7cc88bea4da52896686295fdf0664a80f83ba3c6
prafulf98 <your_email@example.com>
2025-07-31 22:28:37 +0530

    update center list

--- src/json/centerList.js
+++ src/json/centerList.js
@@ -3,17 +3,122 @@ const centerList = [
         taluka: 'Yavatmal',
         information: [
             {
-                centerName: "Ganpati mandir",
-                centerLocation: "google location",
-                centerCityVillage: "Kharda",
+                centerName: "Main Center",
+                centerLocation: "https://maps.app.goo.gl/3fAV1eb5JkZTLuQS6",
+                centerCityVillage: "Yavatmal",
                 centerTaluka: "Yavatmal",
                 centerDist: "YTL",
                 centerDay: "Sunday",
-                centerTime: "7 AM",
-                centerCoOrdinator: "Sunil Kaka",
-                centerCoMobileNo: "12345678900",
-                centerAddress: "Qwertyu Ioas Dfghjkl Zxcvbnjm"
+                centerTime: "05:00 PM",
+                centerCoOrdinator: "Sau. YaminiTai Wandile",
+                centerCoMobileNo: "9404699286",
+                centerAddress: "Shri Ganpati Mandir, Shivneri Society, Arni Road, Yavatmal",
+
             }, 
+            {
+                centerName: "Mahalaxmi Nagar",
+                centerLocation: "https://maps.app.goo.gl/3fAV1eb5JkZTLuQS6",
+                centerCityVillage: "Yavatmal",
+                centerTaluka: "Yavatmal",
+                centerDist: "YTL",
+                centerDay: "Thursday",
+                centerTime: "07:00 PM",
+                centerCoOrdinator: "Sau. Ranjanatai Dhurankar",
+                centerCoMobileNo: "9420020514",
+                centerAddress: "Vitthal Rukmini Temple, Mahalxmi Nagar, Wadgaon, Yavatmal.",
+
+            },
+            {
+                centerName: "Waghapur",
+                centerLocation: "https://maps.app.goo.gl/3fAV1eb5JkZTLuQS6",
+                centerCityVillage: "Yavatmal",
+                centerTaluka: "Yavatmal",
+                centerDist: "YTL",
+                centerDay: "Saturday",
+                centerTime: "05:30 PM",
+                centerCoOrdinator: "Sau. Nishatai Dahake",
+                centerCoMobileNo: "9404453769",
+                centerAddress: "Shree Hinglaj Mata Mandir, Savitribai Fule Society, Waghapur, Yavatmal.",
+
+            },
+            {
+                centerName: "Balaji Society",
+                centerLocation: "https://maps.app.goo.gl/3fAV1eb5JkZTLuQS6",
+                centerCityVillage: "Yavatmal",
+                centerTaluka: "Yavatmal",
+                centerDist: "YTL",
+                centerDay: "Tuesday",
+                centerTime: "05:30 PM",
+                centerCoOrdinator: "Sau. Ritu Chhabada",
+                centerCoMobileNo: "9423297111",
+                centerAddress: "Jeshth Nagrik Bhawan, Near Maharastra Bank, Balaji Society, Yavatmal.",
+
+            },
+            {
+                centerName: "Balshakti Center",
+                centerLocation: "https://maps.app.goo.gl/3fAV1eb5JkZTLuQS6",
+                centerCityVillage: "Yavatmal",
+                centerTaluka: "Yavatmal",
+                centerDist: "YTL",
+                centerDay: "Sunday",
+                centerTime: "09:00 AM",
+                centerCoOrdinator: "Sau. Vandanatai Gedam",
+                centerCoMobileNo: "7798160135",
+                centerAddress: "Dr. Kudmethe's Home, Shivneri Society, Near Arni Road, Yavatmal.",
+
+            }, 
+            {
+                centerName: "YuvaShakti Center",
+                centerLocation: "https://maps.app.goo.gl/3fAV1eb5JkZTLuQS6",
+                centerCityVillage: "Yavatmal",
+                centerTaluka: "Yavatmal",
+                centerDist: "YTL",
+                centerDay: "Wednesday",
+                centerTime: "04:00 PM",
+                centerCoOrdinator: "Rushikesh Koychade",
+                centerCoMobileNo: "7057856640",
+                centerAddress: "Shree Ganesh Mandir, Shivneri Society, Near Arni Road, Yavatmal.",
+
+            },
+            {
+                centerName: "Mahinde Chowk",
+                centerLocation: "https://maps.app.goo.gl/3fAV1eb5JkZTLuQS6",
+                centerCityVillage: "Yavatmal",
+                centerTaluka: "Yavatmal",
+                centerDist: "YTL",
+                centerDay: "Friday",
+                centerTime: "05:30 PM",
+                centerCoOrdinator: "Sau. Pranali Mirzapure",
+                centerCoMobileNo: "9960003843",
+                centerAddress: "Sane Guruji Shala, Near Mahila Bank, Mahinde Chowk, Yavatmal.",
+
+            },
+            {
+                centerName: "Girija Nagar",
+                centerLocation: "https://maps.app.goo.gl/3fAV1eb5JkZTLuQS6",
+                centerCityVillage: "Yavatmal",
+                centerTaluka: "Yavatmal",
+                centerDist: "YTL",
+                centerDay: "Saturday",
+                centerTime: "05:30 PM",
+                centerCoOrdinator: "Shri. Avinash Mesekar",
+                centerCoMobileNo: "9372621883",
+                centerAddress: "Sai Mandir Hall, Near P. Wadhawani Collage, Girija Nagar, Dhamangaon Road, Yavatmal.",
+
+            },
+            {
+                centerName: "Godam Fail",
+                centerLocation: "https://maps.app.goo.gl/3fAV1eb5JkZTLuQS6",
+                centerCityVillage: "Yavatmal",
+                centerTaluka: "Yavatmal",
+                centerDist: "YTL",
+                centerDay: "Sunday",
+                centerTime: "06:00 AM",
+                centerCoOrdinator: "Ku. Sankesh Khandawe",
+                centerCoMobileNo: "8999078443",
+                centerAddress: "Burud Samaj Hall, Near Apsara Talkies, Godam Fail , Yavatmal.",
+
+            },
              
         ]
     },
@@ -21,16 +126,64 @@ const centerList = [
         taluka: 'Babhulgaon',
         information: [
             {
-                centerName: "babhulgaon main center",
+                centerName: "Babhulgaon Main Center",
+                centerLocation: "google location",
+                centerCityVillage: "Babhulgaon",
+                centerTaluka: "Babhulgaon",
+                centerDist: "YTL",
+                centerDay: "Friday",
+                centerTime: "06:00 PM",
+                centerCoOrdinator: "Shri. Gajanan Hote",
+                centerCoMobileNo: "9604093057",
+                centerAddress: " Shri. Vitthal Rukhmini Mandir, Babhulgaon."
+            },
+            {
+                centerName: "Kharda",
                 centerLocation: "google location",
                 centerCityVillage: "Kharda",
                 centerTaluka: "Babhulgaon",
                 centerDist: "YTL",
+                centerDay: "Saturday",
+                centerTime: "06:00 PM",
+                centerCoOrdinator: "Shri. Rupesh Chatarkar",
+                centerCoMobileNo: "9921323020",
+                centerAddress: "Vitthal Rukmini Temple, Kharda."
+            },
+            {
+                centerName: "Sarfali",
+                centerLocation: "google location",
+                centerCityVillage: "Sarfali",
+                centerTaluka: "Babhulgaon",
+                centerDist: "YTL",
                 centerDay: "Sunday",
-                centerTime: "7 AM",
-                centerCoOrdinator: "Sunil Kaka",
+                centerTime: "07:00 PM",
+                centerCoOrdinator: "Sau. Reshmatai Gudadhe",
+                centerCoMobileNo: "7620426078",
+                centerAddress: "Shree Hanuman Temple, Sarfali."
+            },
+            {
+                centerName: "Sawar",
+                centerLocation: "google location",
+                centerCityVillage: "Sawar",
+                centerTaluka: "Babhulgaon",
+                centerDist: "YTL",
+                centerDay: "Monday",
+                centerTime: "05:30 PM",
+                centerCoOrdinator: "Sau. Chandatai Lanjekar",
                 centerCoMobileNo: "12345678900",
-                centerAddress: "Qwertyu Ioas Dfghjkl Zxcvbnjm"
+                centerAddress: "Shree VitthalRukhmini Mandir Hall, At. Po. Sawar, Tq. Babhulgaon"
+            },
+            {
+                centerName: "Asegaon Devi",
+                centerLocation: "google location",
+                centerCityVillage: "Asegaon Devi",
+                centerTaluka: "Babhulgaon",
+                centerDist: "YTL",
+                centerDay: "Tuesday",
+                centerTime: "07:00 PM",
+                centerCoOrdinator: "Rushikesh Koychade",
+                centerCoMobileNo: "7057856640",
+                centerAddress: "Hanuman Mandir Hall, Near Gram Panchayat, At. Po. Asegaon Devi, Tq.Babhulgaon"
             },
         ]
     },
@@ -38,16 +191,16 @@ const centerList = [
         taluka: 'Ner',
         information: [
             {
-                centerName: "Ner",
+                centerName: "Ner Main Center",
                 centerLocation: "google location",
-                centerCityVillage: "Kharda",
+                centerCityVillage: "Ner",
                 centerTaluka: "Ner",
                 centerDist: "YTL",
-                centerDay: "Sunday",
-                centerTime: "7 AM",
-                centerCoOrdinator: "Sunil Kaka",
-                centerCoMobileNo: "12345678900",
-                centerAddress: "Qwertyu Ioas Dfghjkl Zxcvbnjm"
+                centerDay: "Saturday",
+                centerTime: "05:30 PM",
+                centerCoOrdinator: "Sau. Pratibhatai Raut",
+                centerCoMobileNo: "9022995402",
+                centerAddress: "Nagar Parishad Shala No. 2, Marwadi Chowk, Ner."
             }
         ]
     },
@@ -55,16 +208,16 @@ const centerList = [
         taluka: 'Arni',
         information: [
             {
-                centerName: "Arni",
+                centerName: "Arni Main Center",
                 centerLocation: "google location",
-                centerCityVillage: "Kharda",
+                centerCityVillage: "Arni",
                 centerTaluka: "Arni",
                 centerDist: "YTL",
                 centerDay: "Sunday",
-                centerTime: "7 AM",
-                centerCoOrdinator: "Sunil Kaka",
-                centerCoMobileNo: "12345678900",
-                centerAddress: "Qwertyu Ioas Dfghjkl Zxcvbnjm"
+                centerTime: "05:00 PM",
+                centerCoOrdinator: "Sau. Archanatai Butle",
+                centerCoMobileNo: "9673416524",
+                centerAddress: "Nagar Parishad Shala no. 2, Sonar line, Arni, Tq. Arni."
             }
         ]
     },
@@ -72,16 +225,16 @@ const centerList = [
         taluka: 'Darwha',
         information: [
             {
-                centerName: "Darwha",
+                centerName: "Darwha Main Center",
                 centerLocation: "google location",
-                centerCityVillage: "Kharda",
+                centerCityVillage: "Darwha",
                 centerTaluka: "Darwha",
                 centerDist: "YTL",
-                centerDay: "Sunday",
-                centerTime: "7 AM",
-                centerCoOrdinator: "Sunil Kaka",
-                centerCoMobileNo: "12345678900",
-                centerAddress: "Qwertyu Ioas Dfghjkl Zxcvbnjm"
+                centerDay: "Saturday",
+                centerTime: "06:00 PM",
+                centerCoOrdinator: "Sau. Pranitatai Irwe",
+                centerCoMobileNo: "8999558107",
+                centerAddress: "Annabhau Sathe  Hall, Shikshak Colony, Digras Bypass Road, Darwha."
             }
         ]
     },
@@ -89,19 +242,19 @@ const centerList = [
         taluka: 'Pusad',
         information: [
             {
-                centerName: "Pusad",
+                centerName: "Pusad Main Center",
                 centerLocation: "google location",
-                centerCityVillage: "Kharda",
+                centerCityVillage: "Pusad",
                 centerTaluka: "Pusad",
                 centerDist: "YTL",
                 centerDay: "Sunday",
-                centerTime: "7 AM",
-                centerCoOrdinator: "Sunil Kaka",
-                centerCoMobileNo: "12345678900",
-                centerAddress: "Qwertyu Ioas Dfghjkl Zxcvbnjm"
+                centerTime: "05:00 PM",
+                centerCoOrdinator: "Shri. Ramnath Rathod",
+                centerCoMobileNo: "9823042134",
+                centerAddress: "Shree Nirmala Devi Ashram, Nirmala Devi Chowk, Green Park -2, Pusad."
             },
             {
-                centerName: "Pusad 2",
+                centerName: "Pusad Sub Center",
                 centerLocation: "google location",
                 centerCityVillage: "Kharda",
                 centerTaluka: "Pusad",
