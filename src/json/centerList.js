0be4af5e2b5e4f4bf3ada7268ffc330731d8eb7f
prafulf98 <your_email@example.com>
2025-09-06 12:45:04 +0530

    updated center list

--- src/json/centerList.js
+++ src/json/centerList.js
@@ -7,14 +7,13 @@ const centerList = [
                 centerLocation: "https://maps.app.goo.gl/3fAV1eb5JkZTLuQS6",
                 centerCityVillage: "Yavatmal",
                 centerTaluka: "Yavatmal",
-                centerDist: "YTL",
+                centerDist: "Yavatmal",
                 centerDay: "Sunday",
                 centerTime: "05:00 PM",
                 centerCoOrdinator: "Sau. YaminiTai Wandile",
                 centerCoMobileNo: "9404699286",
-                centerAddress: "Shri Ganpati Mandir, Shivneri Society, Arni Road, Yavatmal",
-
-            }, 
+                centerAddress: "Loknayak Bapuji Aney Mahila Mahavidyalaya, Datta chowk",
+            },
             {
                 centerName: "Mahalaxmi Nagar",
                 centerLocation: "https://maps.app.goo.gl/3fAV1eb5JkZTLuQS6",
@@ -66,7 +65,7 @@ const centerList = [
                 centerCoMobileNo: "7798160135",
                 centerAddress: "Dr. Kudmethe's Home, Shivneri Society, Near Arni Road, Yavatmal.",
 
-            }, 
+            },
             {
                 centerName: "YuvaShakti Center",
                 centerLocation: "https://maps.app.goo.gl/3fAV1eb5JkZTLuQS6",
@@ -114,12 +113,23 @@ const centerList = [
                 centerDist: "YTL",
                 centerDay: "Sunday",
                 centerTime: "06:00 AM",
-                centerCoOrdinator: "Ku. Sankesh Khandawe",
+                centerCoOrdinator: "Sankesh Khandawe",
                 centerCoMobileNo: "8999078443",
-                centerAddress: "Burud Samaj Hall, Near Apsara Talkies, Godam Fail , Yavatmal.",
-
+                centerAddress: "Burud Samaj Hall, Near Apsara Talkies, Godam Fail , Yavatmal"
             },
-             
+            {
+                centerName: "Kinhi",
+                centerLocation: "",
+                centerCityVillage: "Kinhi",
+                centerTaluka: "Yavatmal",
+                centerDist: "Yavatmal",
+                centerDay: "Friday",
+                centerTime: "06:00 AM",
+                centerCoOrdinator: "Shri. Gajanan Mehare",
+                centerCoMobileNo: "9767586512",
+                centerAddress: "Hanuman Mandir, At. Po. Kinhi. Tq. Yavatmal"
+            },
+
         ]
     },
     {
@@ -267,6 +277,366 @@ const centerList = [
             }
         ]
     },
+    {
+        taluka: 'Ralegaon',
+        information: [
+            {
+                centerCityVillage: 'Ralegaon',
+                centerName: 'Main Center',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Gowari Samaj Bhawan, Ashtha Road, Ralegaon, Tq. Ralegaon.',
+                centerTime: '6:00 PM',
+                centerDay: 'Monday',
+                centerCoOrdinator: 'Sau. Sarikatai Borsare, Sau. Rekha Kowe',
+                centerCoMobileNo: '8975535892, 8208898723',
+                centerLocation: '',
+                centerTaluka: 'Ralegaon'
+            },
+            {
+                centerCityVillage: 'Parsodi',
+                centerName: 'Parsodi',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Sau. Meenatai Gawande\'s Home, At. Po. Parsodi, Tq. Ralegaon.',
+                centerTime: '6:30 PM',
+                centerDay: 'Saturday',
+                centerCoOrdinator: 'Sau. Meena Tai Gawande',
+                centerCoMobileNo: '7798735960',
+                centerLocation: '',
+                centerTaluka: 'Ralegaon'
+            },
+            {
+                centerCityVillage: 'Ralegaon',
+                centerName: 'Ralegaon Sub-Center',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Ishwar Vidya Mandir, Bahale Lay-Out, Mata Nagar, Ralegaon.',
+                centerTime: '6:00 PM',
+                centerDay: 'Saturday',
+                centerCoOrdinator: 'Sau. Nilimatai Chiwhane, Sau. Vaishalitai Zalke',
+                centerCoMobileNo: '8308854990, 9822784888',
+                centerLocation: '',
+                centerTaluka: 'Ralegaon'
+            },
+            {
+                centerCityVillage: 'Dhotra',
+                centerName: 'Dhotra',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Samaj Mandir At. Po. Dhotra Tq. Ralegaon.',
+                centerTime: '6:30 PM',
+                centerDay: 'Thursday',
+                centerCoOrdinator: 'Sau. Mayatai Gadge',
+                centerCoMobileNo: '8999926075',
+                centerLocation: '',
+                centerTaluka: 'Ralegaon'
+            },
+            {
+                centerCityVillage: 'Khairi',
+                centerName: 'Khairi',
+                centerDist: 'Yavatmal',
+                centerAddress: 'At. Home Sau. Bhagyashritai Kute, At. Khairi, Tq. Ralegaon.',
+                centerTime: '7:00 PM',
+                centerDay: 'Thursday',
+                centerCoOrdinator: 'Sau. Bhagyashritai Kute',
+                centerCoMobileNo: '8806392441',
+                centerLocation: '',
+                centerTaluka: 'Ralegaon'
+            },
+            {
+                centerCityVillage: 'Wadki',
+                centerName: 'Wadki',
+                centerDist: 'Yavatmal',
+                centerAddress: 'At. Home Sau. Bharati Tathe, At. Po. Wadki Tq. Ralegaon.',
+                centerTime: '7:00 PM',
+                centerDay: 'Saturday',
+                centerCoOrdinator: 'Sau. Bhartitai Tathe',
+                centerCoMobileNo: '8459224139',
+                centerLocation: '',
+                centerTaluka: 'Ralegaon'
+            },
+            {
+                centerCityVillage: 'Tirzala',
+                centerName: 'Tirzala',
+                centerDist: 'Yavatmal',
+                centerAddress: 'At. Home Sau. Mamtatai Shende, At. Tirzala Tq. Ralegaon.',
+                centerTime: '6:30 PM',
+                centerDay: 'Monday',
+                centerCoOrdinator: 'Sau. Mamtatai Shende',
+                centerCoMobileNo: '9545964530',
+                centerLocation: '',
+                centerTaluka: 'Ralegaon'
+            },
+            {
+                centerCityVillage: 'Wanoja',
+                centerName: 'Wanoja',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Shri. Hanuman Mandir, At. Wanoja. Tq. Ralegaon',
+                centerTime: '6:30 PM',
+                centerDay: 'Thursday',
+                centerCoOrdinator: 'Sau. Priti Tai Nagpure',
+                centerCoMobileNo: '9657016636',
+                centerLocation: '',
+                centerTaluka: 'Ralegaon'
+            },
+            {
+                centerCityVillage: 'Gujari',
+                centerName: 'Gujari',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Sant. Tukdoji Maharaj Mandir, At. Po. Gujari Tq. Ralegaon',
+                centerTime: '6:30 PM',
+                centerDay: 'Tuesday',
+                centerCoOrdinator: 'Sau. Ratnatai Madavi, Sau. Awghane Tai',
+                centerCoMobileNo: '7020857324',
+                centerLocation: '',
+                centerTaluka: 'Ralegaon'
+            },
+            {
+                centerCityVillage: 'Antargaon',
+                centerName: 'Antargaon',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Shri. Hanuman Mandir, At. Antargaon Tq. Ralegaon',
+                centerTime: '7:00 PM',
+                centerDay: 'Wednesday',
+                centerCoOrdinator: 'Sau. Waghmare Tai',
+                centerCoMobileNo: '9021386956',
+                centerLocation: '',
+                centerTaluka: 'Ralegaon'
+            },
+            {
+                centerCityVillage: 'Gujari',
+                centerName: 'Gujari Sub-Center',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Shri. Hanuman Mandir, At. Po. Gujari Tq. Ralegaon.',
+                centerTime: '4:00 PM',
+                centerDay: 'Friday',
+                centerCoOrdinator: 'Sau. Shubhangi Warade',
+                centerCoMobileNo: '9226013406',
+                centerLocation: '',
+                centerTaluka: 'Ralegaon'
+            },
+            {
+                centerCityVillage: 'Pimpalgaon',
+                centerName: 'Pimpalgaon',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Shri. Mahadeo Mandir, At. Po. Pimpalgaon Tq. Ralegaon.',
+                centerTime: '7:00 PM',
+                centerDay: 'Monday',
+                centerCoOrdinator: 'Sau Kantatai Kathale',
+                centerCoMobileNo: '7821044936',
+                centerLocation: '',
+                centerTaluka: 'Ralegaon'
+            },
+            {
+                centerCityVillage: 'Chachora',
+                centerName: 'Chachora',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Zilla Parishad School, At. Chahchora, Tq. Ralegaon.',
+                centerTime: '7:00 PM',
+                centerDay: 'Saturday',
+                centerCoOrdinator: 'Sau. Kalpana Tai Lengure',
+                centerCoMobileNo: '9579368372',
+                centerLocation: '',
+                centerTaluka: 'Ralegaon'
+            }
+        ]
+    },
+    {
+        taluka: 'Pandharkawada',
+        information: [
+            {
+                centerCityVillage: 'Mohada',
+                centerName: 'Latani Nagar',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Near Zilla Parishad School, Latani Nagar, At. Po. Mohada',
+                centerTime: '5:00 PM',
+                centerDay: 'Saturday',
+                centerCoOrdinator: 'Shri. Prakash Gujar',
+                centerCoMobileNo: '8329264196',
+                centerLocation: '',
+                centerTaluka: 'Pandharkawada'
+            },
+            {
+                centerCityVillage: 'Kawatha (Wara)',
+                centerName: 'Kawatha (Wara)',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Zilla Parishad School, At. Kawatha (Wara), Tq. Pandharkawada.',
+                centerTime: '9:00 AM',
+                centerDay: 'Sunday',
+                centerCoOrdinator: 'Ku. Vamshi Madavi',
+                centerCoMobileNo: '8788731975',
+                centerLocation: '',
+                centerTaluka: 'Pandharkawada'
+            },
+            {
+                centerCityVillage: 'Zuli',
+                centerName: 'Zuli',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Hanuman Mandir, At. Zuli, Po. Pahapal, Tq. Pandharkawada.',
+                centerTime: '7:30 AM',
+                centerDay: 'Monday',
+                centerCoOrdinator: 'Nilesh Todsam',
+                centerCoMobileNo: '7499604399',
+                centerLocation: '',
+                centerTaluka: 'Pandharkawada'
+            },
+            {
+                centerCityVillage: 'Kawatha (Wara)',
+                centerName: 'Kawatha (Wara)',
+                centerDist: 'Yavatmal',
+                centerAddress: 'At. Shri Vithobaji chittalwar Home, At Po. Kawatha (Wara) Tq. Pandharkawada.',
+                centerTime: '8:00 PM',
+                centerDay: 'Thursday',
+                centerCoOrdinator: 'Shri. Shubham Nehare',
+                centerCoMobileNo: '9370512427',
+                centerLocation: '',
+                centerTaluka: 'Pandharkawada'
+            },
+            {
+                centerCityVillage: 'Kawatha (Wara)',
+                centerName: 'Kawatha (Wara)',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Gram Panchayat Karyalaya, At. Po. Kawatha (Wara), Tq. Pandharkawada',
+                centerTime: '8:00 PM',
+                centerDay: 'Monday',
+                centerCoOrdinator: 'Sau. Poojatai Gedam, Ku. Sakshi Muke',
+                centerCoMobileNo: '8329678675',
+                centerLocation: '',
+                centerTaluka: 'Pandharkawada'
+            },
+            {
+                centerCityVillage: 'Khairgaon',
+                centerName: 'Khairgaon',
+                centerDist: 'Yavatmal',
+                centerAddress: 'At. Shri Junghare Home, At. Po. Khairgaon, Tq. Pandharkawada',
+                centerTime: '8:00 PM',
+                centerDay: 'Friday',
+                centerCoOrdinator: 'Sau. Pushpatai Junghare, Ku. Ankita Anmulwar',
+                centerCoMobileNo: '',
+                centerLocation: '',
+                centerTaluka: 'Pandharkawada'
+            },
+            {
+                centerCityVillage: 'Tadumari',
+                centerName: 'Tadumari',
+                centerDist: 'Yavatmal',
+                centerAddress: 'At. Shri Ravindra Bhoyar Home, At. Po. Tadumari, Tq. Pandharkawada',
+                centerTime: '7:00 PM',
+                centerDay: 'Thursday',
+                centerCoOrdinator: 'Shri. Shende Bhau',
+                centerCoMobileNo: '',
+                centerLocation: '',
+                centerTaluka: 'Pandharkawada'
+            },
+            {
+                centerCityVillage: 'Belori',
+                centerName: 'Belori',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Vitthal Mandir, At. Po. Belori, Tq. Pandharkawada',
+                centerTime: '8:00 PM',
+                centerDay: 'Saturday',
+                centerCoOrdinator: 'Sau. Jayashri Gaurkar, Sau. Sunita Kamatwar',
+                centerCoMobileNo: '9325148337, 7620546379',
+                centerLocation: '',
+                centerTaluka: 'Pandharkawada'
+            }
+        ]
+    },
+    {
+        taluka: 'Ghatanji',
+        information: [
+            {
+                centerCityVillage: 'Ghatanji',
+                centerName: 'Ghatanji',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Jivan Vikas Marathi School, Infront of Tanishq Jwellers, Yavatmal Road, Ghatanji',
+                centerTime: '5:00 PM',
+                centerDay: 'Saturday',
+                centerCoOrdinator: 'Sau. Chandatai Gharat, Sau. Venutai Chandekar',
+                centerCoMobileNo: '7218299357, 7248910272',
+                centerLocation: '',
+                centerTaluka: 'Ghatanji'
+            },
+            {
+                centerCityVillage: 'Ghatanji',
+                centerName: 'Ghatanji Sub-Center',
+                centerDist: 'Yavatmal',
+                centerAddress: 'L. S. D. Hall, Near SBI Bank, Ghatanji.',
+                centerTime: '4:00 PM',
+                centerDay: 'Sunday',
+                centerCoOrdinator: 'Sau. Seema Bawane, Sau. Ashatai Dhawale',
+                centerCoMobileNo: '9022281885',
+                centerLocation: '',
+                centerTaluka: 'Ghatanji'
+            }
+        ]
+    },
+    {
+        taluka: 'Mahagaon',
+        information: [
+            {
+                centerCityVillage: 'Mahagaon',
+                centerName: 'Old Bus Stop',
+                centerDist: 'Yavatmal',
+                centerAddress: "Shri. Tajane's Home, At. Po. Mahagaon Kasaba. Tq. Darwha.",
+                centerTime: '6:30 PM',
+                centerDay: 'Friday',
+                centerCoOrdinator: 'Shri. Sanjay Tajne, Shri. Bhimrao Darekar',
+                centerCoMobileNo: '8975111183, 8888629231',
+                centerLocation: '',
+                centerTaluka: 'Mahagaon'
+            },
+        ]
+    },
+    {
+        taluka: 'Umarkhed',
+        information: [
+            {
+                centerCityVillage: 'Hanuman Ward',
+                centerName: 'Hanuman Ward',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Seva Coaching Classes, Sevadas Nagar, Infront of Shri Jagdamba Temple, Umarkhed, Tq. Umarkhed',
+                centerTime: '8:00 AM',
+                centerDay: 'Sunday',
+                centerCoOrdinator: 'Sau. Sharadatai Arya, Shri. Santosh Khokale',
+                centerCoMobileNo: '9763794513, 9404235375',
+                centerLocation: '',
+                centerTaluka: 'Umarkhed'
+            }
+        ]
+    },
+    {
+        taluka: 'Digras',
+        information: [
+            {
+                centerCityVillage: 'Digras',
+                centerName: 'Digras',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Hanuman Mandir, Gurudev Nagar, Old Darwha Road, Digras.',
+                centerTime: '5:30 PM',
+                centerDay: 'Saturday',
+                centerCoOrdinator: 'Sau. Karunatai Gurad',
+                centerCoMobileNo: '9322234985',
+                centerLocation: '',
+                centerTaluka: 'Digras',
+            }
+        ]
+    },
+    {
+        taluka: 'Kalamb',
+        information: [
+            {
+                centerCityVillage: 'Kalamb',
+                centerName: 'Kalamb',
+                centerDist: 'Yavatmal',
+                centerAddress: 'Shri. RadhaKrishna Mandir Hall, Agrawal Lay-Out, Rasa Road, Kalamb.',
+                centerTime: '5:00 PM',
+                centerDay: 'Saturday',
+                centerCoOrdinator: 'Sau. Manjushatai Vidhate',
+                centerCoMobileNo: '7875275171',
+                centerLocation: '',
+                centerTaluka: 'Kalamb',
+            }
+        ]
+    },
+
 ]
 
 export default centerList
